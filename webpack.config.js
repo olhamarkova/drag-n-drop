1f702f12e11085ce5c5a62c047e5c33d047ddeb9
olhamarkova <olga.mulchenko@gmail.com>
2024-12-01 12:20:18 +0200

    fix dir name

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     publicPath: "/dist/",
   },
   devtool: "inline-source-map",
