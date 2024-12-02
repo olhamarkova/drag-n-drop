76674a90c59edbd3a7bb29b5602bbeab57e2357a
Olha Markova <120654932+olhamarkova@users.noreply.github.com>
2024-12-02 18:46:44 +0200

    Add e2e tests (#2)

---
+++ tests/tests.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from "@playwright/test";
+import { ProjectManagerPage } from "./support/page";
+
+let app: ProjectManagerPage;
+
+test.beforeEach(async ({ page }) => {
+  app = new ProjectManagerPage(page);
+  await page.goto("/");
+});
+
+test.afterAll(async () => {
+  await app.page.close();
+});
+
+test.describe("Drag&Drop Project Tests", async () => {
+  test("User shall be able to add a new project", async () => {
+    await app.addProject("Test project", "My first project", 5);
+
+    await expect(app.projectItem("active")).toBeVisible();
+    await expect(app.projectItem("active")).toHaveCount(1);
+  });
+
+  test("User shall be able to drag and drop a project", async () => {
+    await app.addProject("Test project", "My second project", 6);
+    await app.dragAndDrop("active", "finished");
+
+    await expect(app.projectItem("finished")).toBeVisible();
+    await expect(app.projectItem("finished")).toHaveCount(1);
+    await expect(app.projectItem("active")).not.toBeVisible();
+  });
+
+  test("User shall see an error when provides too short description", async () => {
+    await app.page.on("dialog", async (dialog) => {
+      expect(dialog.message()).toBe("Invalid input value. Please try again");
+      await dialog.accept();
+    });
+    await app.addProject("Test project 3", "re", 6);
+  });
+});
